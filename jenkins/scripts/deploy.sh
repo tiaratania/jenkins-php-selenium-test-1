ecaf843a70bd7b922fbd41f744ae61d70e3c901b
tiara <tiarataniaa@gmail.com>
2024-07-21 21:25:22 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 set -x
+echo 'Now...'
 docker run -d --network jenkins-php-selenium-test-1_jenkins-net -p 80:80 --name my-apache-php-app -v /Users/tiara/Documents/GitHub/jenkins-php-selenium-test-1/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
