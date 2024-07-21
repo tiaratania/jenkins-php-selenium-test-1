baf4157f7907f2787f42675261d40760dcd8cf2e
tiara <tiarataniaa@gmail.com>
2024-07-21 20:35:21 +0800

    update

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env sh
 
 set -x
-docker run -d -p 80:80 --name my-apache-php-app -v c:\\...\\jenkins-php-selenium-test\\src:/var/www/html php:7.2-apache
+docker rm -f my-apache-php-app || true
+docker run -d --network jenkins-php-selenium-test_jenkins-net -p 80:80 --name my-apache-php-app -v /Users/tiara/Documents/GitHub/jenkins-php-selenium-test/src:/var/www/html php:7.2-apache
 sleep 1
 set +x
 
