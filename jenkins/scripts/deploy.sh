1a6f05f25ca1ce4aa9138fbb47663490bd8b92d7
tiara <tiarataniaa@gmail.com>
2024-07-23 00:57:13 +0800

    test

--- jenkins/scripts/deploy.sh
+++ jenkins/scripts/deploy.sh
@@ -2,8 +2,15 @@
 
 set -x
 echo 'Now...'
-docker run -d --network jenkins-php-selenium-test-1_jenkins-net -p 80:80 --name my-apache-php-app -v /Users/tiara/Documents/GitHub/jenkins-php-selenium-test-1/src:/var/www/html php:7.2-apache
+if docker run -d --network jenkins-php-selenium-test-1_jenkins-net -p 84:80 --name my-apache-php-app -v /Users/tiara/Documents/GitHub/jenkins-php-selenium-test-1/src:/var/www/html php:7.2-apache; then
+  echo 'Apache container started successfully.'
+else
+  echo 'Failed to start Apache container.'
+  exit 1
+fi
+
 sleep 1
+
 set +x
 
 echo 'Now...'
